0c82f1bf6dcd9188553e5c8e8f9735e182ea9a9f
Serge Block <block@live.ru>
2022-08-23 20:21:34 +0200

    json versucht ist aber nicht schneller.
    Signed-off-by: Serge Block <block@live.ru>

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.autoscout24.de/lst/volkswagen/caddy/ve_maxi?fregfrom=2010&sort=age&desc=1&cy=D&atype=C&ustate=N%2CU&powertype=kw&ocs_listing=include&amp%3Bdesc=0&amp%3Bustate=N%2CU&amp%3Batype=C&amp%3Bcy=D&amp%3Bocs_listing=include&amp%3Bfregfrom=2010&search_id=qf54cm5goi&page='
+url = 'https://www.autoscout24.de/lst/volkswagen/caddy/ve_maxi?fregfrom=2010&sort=age&desc=1&cy=D&zipr=150&zip=53117%20Zentrum%20%28Bonn%29&lon=7.0709135&lat=50.7578523&atype=C&ustate=N%2CU&powertype=kw&priceto=17500&ocs_listing=include&amp%3Bdesc=0&amp%3Bustate=N%2CU&amp%3Batype=C&amp%3Bcy=D&amp%3Bocs_listing=include&amp%3Bfregfrom=2010&search_id=49pe6bnzx8&page='
 
 chat_id = '142106628'
 
@@ -93,28 +93,14 @@ def get_ids(path):
 # while open('alt_product_list.json',  'r') as file:
 #         alt_product_list = json.load(file)
 
-
-
-if __name__ == '__main__':
+def main():
+    global list_url
     for i in range(1,21):
         list_url = url + str(i)
         print('SOUP')
         soup = get_data(list_url)
         print('parse')
         parse(soup)
-
-    # with open('alt_product_list.json') as file:
-    #     alt_product_list = json.load(file)
-    #     file.close()
-    #
-    #
-    # for i in products_list:
-    #     for j in alt_product_list:
-    #         if i['ID'] != j['ID']:
-    #             send_message(chat_id, i['href'])
-
-
-
 
 
     ids = get_ids('alt_product_list.csv')
@@ -126,4 +112,9 @@ if __name__ == '__main__':
     save(products_list, 'alt_product_list.csv')
 
     # save_json(products_list,'alt_product_list.json')
+
+
+
+if __name__ == '__main__':
+    main()
 
